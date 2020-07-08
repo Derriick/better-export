7e7c3e2aaef154091115a8f199dc4af0e74bfff1
Derriick <derriick.ensiie@yahoo.com>
2020-07-08 10:24:18 +0200

    Add logging with verbose option

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use clap::{App, Arg, ArgMatches};
 use ini::{ini::EscapePolicy, ini::Properties, Ini};
+use log::{debug, error};
 use std::{
 	env, fs, io,
 	path::{Path, PathBuf},
@@ -27,38 +28,46 @@ pub fn create_app() -> App<'static, 'static> {
 			  .short("c")
 			  .long("config")
 			  .value_name("FILE")
-			  .help(concat!("Sets the path of the configuration file to use. Default is '", default_conf!(), "'."))
+			  .help(concat!("Set the path of the configuration file to use. Default is '", default_conf!(), "'."))
 			  .takes_value(true))
 		.arg(Arg::with_name("path")
 			  .short("p")
 			  .long("path")
 			  .value_name("FILE")
-			  .help("Sets the export path. {DATE} will be replaced by the current local date/time with the choosen format.\nIf no extension is specified, the extension of the input file will be used.")
+			  .help("Set the export path. {DATE} will be replaced by the current local date/time with the choosen format.\nIf no extension is specified, the extension of the input file will be used.")
 			  .takes_value(true))
 		.arg(Arg::with_name("date")
 			  .short("d")
 			  .long("date")
 			  .value_name("FORMAT")
-			  .help("Sets the date format to use in the {DATE} placeholder.")
+			  .help("Set the date format to use in the {DATE} placeholder.")
 		     .takes_value(true))
-		.arg(Arg::with_name("path-default")
-			  .long("path-default")
+		.arg(Arg::with_name("default-path")
+			  .long("default-path")
 			  .value_name("FILE")
-			  .help("Sets the default export path.")
+			  .help("Set the default export path.")
 			  .takes_value(true))
 		.arg(Arg::with_name("reset")
 		     .long("reset")
 		     .help("Reset the export path to the default export path."))
+		.arg(Arg::with_name("verbose")
+		     .short("v")
+		     .multiple(true)
+		     .help("Set the level of verbosity."))
 }
 
-pub fn move_file(from: &Path, to: &Path) -> io::Result<()> {
+pub fn move_file<P: AsRef<Path>, Q: AsRef<Path>>(
+	from: P,
+	to: Q,
+) -> io::Result<()> {
+	let from = Path::new(from.as_ref());
 	if from.is_file() {
 		match fs::rename(from, &to) {
 			Ok(_) => Ok(()),
-			Err(_) => match fs::copy(from, &to) {
-				Ok(_) => fs::remove_file(from),
-				Err(err) => Err(err),
-			},
+			Err(_) => {
+				fs::copy(from, &to)?;
+				fs::remove_file(from)
+			}
 		}
 	} else {
 		Err(io::Error::from(io::ErrorKind::InvalidInput))
@@ -71,10 +80,10 @@ pub fn get_conf(matches: &ArgMatches) -> io::Result<Ini> {
 		None => {
 			let path = Path::new(DEFAULT_CONF);
 			if path.is_relative() {
-				let dir = env::current_exe()?;
-				let dir = dir.parent().expect(&format!(
+				let path_exe = env::current_exe()?;
+				let dir = path_exe.parent().expect(&format!(
 					"Current EXE file '{}' has no parent",
-					dir.to_str().unwrap_or("???")
+					path_exe.to_str().unwrap_or("???")
 				));
 				dir.join(path)
 			} else {
@@ -83,13 +92,13 @@ pub fn get_conf(matches: &ArgMatches) -> io::Result<Ini> {
 		}
 	};
 
-	println!("config:  {}", path.to_str().unwrap());
+	debug!("config:  {}", path.to_str().unwrap_or("None"));
 
 	let mut conf = Ini::load_from_file_noescape(&path).unwrap_or(Ini::new());
 	update_ini(&mut conf, &matches);
 
 	if let Err(err) = conf.write_to_file_policy(&path, EscapePolicy::Nothing) {
-		eprintln!(
+		error!(
 			"Failed to write configuration to file {}\n{}",
 			path.to_str().unwrap_or("???"),
 			err
@@ -102,18 +111,18 @@ pub fn get_conf(matches: &ArgMatches) -> io::Result<Ini> {
 fn update_ini(ini: &mut Ini, matches: &ArgMatches) {
 	if ini.section(None::<String>).is_none() {
 		ini.with_general_section()
-			.set("path-default", DEFAULT_PATH)
+			.set("default-path", DEFAULT_PATH)
 			.set("path", DEFAULT_PATH)
 			.set("date", DEFAULT_DATE);
 	}
 	let mut section = ini.general_section_mut();
 
-	update_value(&mut section, "path-default", matches);
+	update_value(&mut section, "default-path", matches);
 	update_value(&mut section, "date", matches);
 
 	if matches.is_present("reset") {
 		let path = section.clone();
-		let path = path.get("path-default").unwrap();
+		let path = path.get("default-path").unwrap();
 		section.insert("path", path);
 	} else {
 		update_value(&mut section, "path", matches);
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use chrono::prelude::*;
+use env_logger;
+use log::{debug, info, LevelFilter};
 use std::{io, path::Path, path::PathBuf};
 
 use better_export::*;
 
 fn main() -> io::Result<()> {
 	let matches = create_app().get_matches();
+
+	let level_filter = match matches.occurrences_of("verbose") {
+		//0 => LevelFilter::Error,
+		0 => LevelFilter::Warn,
+		1 => LevelFilter::Info,
+		2 => LevelFilter::Debug,
+		_ => LevelFilter::Trace,
+	};
+	env_logger::builder().filter_level(level_filter).init();
+
 	let conf = get_conf(&matches)?;
 	let conf = conf.general_section();
 
-	println!("file:    {}", matches.value_of("file").unwrap_or("no file"));
-	println!("default: {}", conf.get("path-default").unwrap_or("None"));
-	println!("path:    {}", conf.get("path").unwrap_or("None"));
-	println!("date:    {}", conf.get("date").unwrap_or("None"));
-	println!("reset:   {}", matches.is_present("reset"));
+	debug!("file:    {}", matches.value_of("file").unwrap_or("None"));
+	debug!("default: {}", conf.get("default-path").unwrap_or("None"));
+	debug!("path:    {}", conf.get("path").unwrap_or("None"));
+	debug!("date:    {}", conf.get("date").unwrap_or("None"));
+	debug!("reset:   {}", matches.is_present("reset"));
 
 	match matches.value_of("file") {
 		Some(src) => {
@@ -34,7 +46,7 @@ fn main() -> io::Result<()> {
 				}
 			}
 
-			println!(
+			info!(
 				"Move '{}' to '{}'",
 				&src.to_str().unwrap_or("???"),
 				&dst.to_str().unwrap_or("???")
